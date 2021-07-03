ce0737cd361217d13a9eb4092ab5a50ff86afde7
adizh <83157462+adizh@users.noreply.github.com>
2021-07-03 13:14:38 +0600

    Update index.js

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,9 +1,9 @@
-import { createStore, compose} from 'redux';
+import {createStore,compose,applyMiddleware} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
 
-import reducers from './reducers/index';
-
-const initialState = {};
-
-const store = createStore(reducers, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
+const composeFunk = process.env.NODE_ENV === 'development' ? composeWithDevTools : compose;
+const composeEnchanters = composeFunk(applyMiddleware(thunk));
+const store = createStore(reducers, composeEnchanters);
 export default store;
